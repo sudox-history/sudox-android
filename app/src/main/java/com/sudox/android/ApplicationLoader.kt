67c91465a7b38ce17e17c2d6acf8d9f1f28c69da
themax <maksim182003mit@gmail.com>
2018-08-09 01:19:39 +0300

    [FEATURES] Installed LeakCanary

--- app/src/main/java/com/sudox/android/ApplicationLoader.kt
+++ app/src/main/java/com/sudox/android/ApplicationLoader.kt
@@ -1,5 +1,6 @@
 package com.sudox.android
 
+import com.squareup.leakcanary.LeakCanary
 import com.sudox.android.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
@@ -16,6 +17,14 @@ class ApplicationLoader : DaggerApplication() {
 
     override fun onCreate() {
         super.onCreate()
+
+        // This process is dedicated to LeakCanary for heap analysis.
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return
+        }
+
+        // Install leak canary
+        LeakCanary.install(this)
 
         // Enable Timber
         if (BuildConfig.DEBUG) {
