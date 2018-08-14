3c5136ec9a550ec27a0a62112fb6b0ea3fe3767c
anton-pc <kerjen01@gmail.com>
2018-08-14 22:16:51 +0300

    [FEATURE] Added glide and remove fresco

--- app/src/main/java/com/sudox/android/ApplicationLoader.kt
+++ app/src/main/java/com/sudox/android/ApplicationLoader.kt
@@ -1,7 +1,5 @@
 package com.sudox.android
 
-import com.facebook.drawee.backends.pipeline.Fresco
-import com.squareup.leakcanary.LeakCanary
 import com.sudox.android.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
@@ -18,17 +16,6 @@ class ApplicationLoader : DaggerApplication() {
 
     override fun onCreate() {
         super.onCreate()
-
-        // This process is dedicated to LeakCanary for heap analysis.
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            return
-        }
-
-        // Install leak canary
-        LeakCanary.install(this)
-
-        // Install Fresco
-        Fresco.initialize(this)
 
         // Enable Timber
         if (BuildConfig.DEBUG) {
--- app/src/main/java/com/sudox/android/ui/adapters/ContactsAdapter.kt
+++ app/src/main/java/com/sudox/android/ui/adapters/ContactsAdapter.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.sudox.android.R
 import com.sudox.android.database.Contact
 import kotlinx.android.synthetic.main.card_contact.view.*
@@ -29,8 +30,10 @@ class ContactsAdapter(var items: List<Contact>,
 
             val text = "${items[position].name.split(" ")[0][0]}${items[position].name.split(" ")[1][0]}"
 
-            holder.avatar.setImageBitmap(setGradientColor(items[position].firstColor!!,
-                    items[position].secondColor!!, text))
+            Glide.with(context)
+                    .load(setGradientColor(items[position].firstColor!!,
+                            items[position].secondColor!!, text)).into(holder.avatar)
+
             //TODO:refactor this code
         } else {
 
@@ -41,13 +44,13 @@ class ContactsAdapter(var items: List<Contact>,
     }
 
     private fun setGradientColor(firstColor: String, secondColor: String, text: String): Bitmap {
-        val imageBitmap = Bitmap.createBitmap(300, 300, Bitmap.Config.ARGB_8888)
+        val imageBitmap = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888)
         val imageCanvas = Canvas(imageBitmap)
         val paint = getGradientPaint(firstColor, secondColor)
         imageCanvas.drawCircle((imageBitmap.width / 2).toFloat(), (imageBitmap.height / 2).toFloat(), 180f, paint)
         paint.shader = null
         paint.color = Color.parseColor("#ffffff")
-        drawTextCentred(imageCanvas, paint, text, 100, (imageBitmap.width / 2).toFloat(), (imageBitmap.height / 2).toFloat())
+        drawTextCentred(imageCanvas, paint, text, 60, (imageBitmap.width / 2).toFloat(), (imageBitmap.height / 2).toFloat())
 
         return imageBitmap
     }
