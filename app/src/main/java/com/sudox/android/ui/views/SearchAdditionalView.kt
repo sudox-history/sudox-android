f098df521bc10e1218ec38547dd0e53a0124056b
themax <maksim182003mit@gmail.com>
2018-08-18 00:35:55 +0300

    [FEATURE] Improved hiding/showing animation of SearchAdditionalView

--- app/src/main/java/com/sudox/android/ui/views/SearchAdditionalView.kt
+++ app/src/main/java/com/sudox/android/ui/views/SearchAdditionalView.kt
@@ -2,11 +2,10 @@ package com.sudox.android.ui.views
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.animation.AccelerateDecelerateInterpolator
 import android.view.animation.AccelerateInterpolator
 import android.view.animation.DecelerateInterpolator
 import android.widget.RelativeLayout
-import androidx.transition.ChangeBounds
-import androidx.transition.TransitionSet
 import com.sudox.android.R
 
 class SearchAdditionalView(context: Context, attrs: AttributeSet) : RelativeLayout(context, attrs) {
@@ -15,7 +14,8 @@ class SearchAdditionalView(context: Context, attrs: AttributeSet) : RelativeLayo
     var visible: Boolean = false
     var animator = animate()
             .setStartDelay(0)
-            .setDuration(200)
+            .setDuration(300)
+            .setInterpolator(AccelerateDecelerateInterpolator())
 
     init {
         inflate(context, R.layout.include_search_navbar_addition, this)
@@ -24,22 +24,18 @@ class SearchAdditionalView(context: Context, attrs: AttributeSet) : RelativeLayo
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
 
-        measureHeight = MeasureSpec.getSize(heightMeasureSpec)
-
         // Hide this view if bottom padding is negative
         if (!visible) {
-            y = -measureHeight.toFloat()
+            translationY = -height.toFloat()
         }
     }
 
     fun toggle() {
         visible = if (!visible) {
-            animator.interpolator = DecelerateInterpolator()
             animator.translationY(0F)
             true
         } else {
-            animator.interpolator = AccelerateInterpolator()
-            animator.translationY(-measureHeight.toFloat())
+            animator.translationY(-height.toFloat())
             false
         }
     }
