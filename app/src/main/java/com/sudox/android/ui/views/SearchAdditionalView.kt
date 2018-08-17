aa9c4b3ecf8b654801e84dac42797feadd3e0ab9
themax <maksim182003mit@gmail.com>
2018-08-17 20:54:08 +0300

    * Addition for previous commit *

--- app/src/main/java/com/sudox/android/ui/views/SearchAdditionalView.kt
+++ app/src/main/java/com/sudox/android/ui/views/SearchAdditionalView.kt
@@ -1,21 +1,20 @@
 package com.sudox.android.ui.views
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.view.ViewGroup
 import android.view.animation.DecelerateInterpolator
 import android.widget.RelativeLayout
 import androidx.transition.ChangeBounds
 import androidx.transition.TransitionManager
 import androidx.transition.TransitionSet
-import com.google.android.material.appbar.AppBarLayout
+import androidx.transition.Visibility
 import com.sudox.android.R
 
 class SearchAdditionalView(context: Context, attrs: AttributeSet) : RelativeLayout(context, attrs) {
 
     var measureHeight: Int = 0
-    lateinit var marginLayoutParams: MarginLayoutParams
     var visible: Boolean = false
 
     private val transitionSet: TransitionSet by lazy {
@@ -34,7 +33,6 @@ class SearchAdditionalView(context: Context, attrs: AttributeSet) : RelativeLayo
         inflate(context, R.layout.include_search_navbar_addition, this)
     }
 
-    @SuppressLint("DrawAllocation")
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
 
@@ -42,23 +40,19 @@ class SearchAdditionalView(context: Context, attrs: AttributeSet) : RelativeLayo
 
         // Hide this view if bottom padding is negative
         if (!visible) {
-            marginLayoutParams = MarginLayoutParams(layoutParams)
-            marginLayoutParams.bottomMargin = -measureHeight
+            setPadding(0, 0, 0, -measureHeight)
         }
-        layoutParams = AppBarLayout.LayoutParams(marginLayoutParams)
     }
 
     fun toggle() {
         TransitionManager.beginDelayedTransition(parent as ViewGroup, transitionSet)
 
         visible = if (!visible) {
-            marginLayoutParams.setMargins(0, 0, 0, 0)
+            setPadding(0, 0, 0, 0)
             true
         } else {
-            marginLayoutParams.setMargins(0, 0, 0, -measureHeight)
-
+            setPadding(0, 0,0, -measureHeight)
             false
         }
-        layoutParams = AppBarLayout.LayoutParams(marginLayoutParams)
     }
 }
