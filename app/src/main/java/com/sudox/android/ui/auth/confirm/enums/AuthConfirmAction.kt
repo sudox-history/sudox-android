1f3dc4b92d3ba3b39099a6d8525b526dec818d64
TheMax <maksim182003mit@gmail.com>
2018-09-23 00:33:56 +0300

    Small changes.

--- app/src/main/java/com/sudox/android/ui/auth/confirm/enums/AuthConfirmAction.kt
+++ app/src/main/java/com/sudox/android/ui/auth/confirm/enums/AuthConfirmAction.kt
@@ -2,6 +2,7 @@ package com.sudox.android.ui.auth.confirm.enums
 
 enum class AuthConfirmAction {
     SHOW_ERROR,
-    SHOW_REGISTRATION,
+    SHOW_REGISTER_FRAGMENT,
+    SHOW_EMAIL_FRAGMENT_WITH_CODE_EXPIRED_ERROR,
     FREEZE
 }
