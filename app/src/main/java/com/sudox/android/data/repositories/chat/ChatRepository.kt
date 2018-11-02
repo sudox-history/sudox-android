ca4180285e81f9b7b1b8a78479c35423714b4f93
TheMax <maksim182003mit@gmail.com>
2018-11-02 22:33:09 +0300

    Trying to recovery receiving chat messages.

--- app/src/main/java/com/sudox/android/data/repositories/chat/ChatRepository.kt
+++ app/src/main/java/com/sudox/android/data/repositories/chat/ChatRepository.kt
@@ -1,10 +1,15 @@
 package com.sudox.android.data.repositories.chat
 
 import android.arch.lifecycle.LiveData
-import com.sudox.android.data.database.dao.UserChatMessagesDao
-import com.sudox.android.data.database.model.UserChatMessage
-import com.sudox.android.data.models.chats.dto.SendUserChatMessageDTO
+import com.sudox.android.data.database.dao.ChatMessagesDao
+import com.sudox.android.data.database.model.ChatMessage
+import com.sudox.android.data.models.chats.dto.NewChatMessageNotifyDTO
+import com.sudox.android.data.models.chats.dto.SendChatMessageDTO
+import com.sudox.android.data.repositories.auth.AccountRepository
+import com.sudox.android.data.repositories.auth.AuthRepository
 import com.sudox.protocol.ProtocolClient
+import kotlinx.coroutines.experimental.GlobalScope
+import kotlinx.coroutines.experimental.async
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -13,24 +18,44 @@ const val MESSAGE_FROM = 1
 
 @Singleton
 class ChatRepository @Inject constructor(private val protocolClient: ProtocolClient,
-                                         private val userChatMessagesDao: UserChatMessagesDao) {
+                                         private val authRepository: AuthRepository,
+                                         private val accountRepository: AccountRepository,
+                                         private val chatMessagesDao: ChatMessagesDao) {
+
+    private var senderId: String? = null
 
     init {
-//        protocolClient.listenMessage<>()
+        authRepository.accountSessionLiveData.observeForever {
+            GlobalScope.async {
+                senderId = if (it != null) {
+                    accountRepository.getAccount().await()!!.id
+                } else {
+                    null
+                }
+            }
+        }
+
+        protocolClient.listenMessage<NewChatMessageNotifyDTO>("notify.chats.new") {
+            chatMessagesDao.insertOne(ChatMessage(it.id, it.sender, it.peer, it.message, it.date, if (it.peer == senderId) {
+                MESSAGE_FROM
+            } else {
+                MESSAGE_TO
+            }))
+        }
     }
 
     fun sendSimpleMessage(peerId: String, message: String) {
-        protocolClient.makeRequest<SendUserChatMessageDTO>("chats.send", SendUserChatMessageDTO().apply {
+        protocolClient.makeRequest<SendChatMessageDTO>("chats.send", SendChatMessageDTO().apply {
             this.peerId = peerId
             this.message = message
         }) {
             if (it.containsError()) return@makeRequest
 
             // Save messages to database
-            userChatMessagesDao.insertOne(UserChatMessage(it.messageId, message, it.date, MESSAGE_TO, peerId))
+            chatMessagesDao.insertOne(ChatMessage(it.messageId, senderId!!, it.peerId, message, it.date, MESSAGE_TO))
         }
     }
 
-    fun observeWithCurrentRecipient(recipientId: String): LiveData<List<UserChatMessage>>
-            = userChatMessagesDao.loadMessagesWithRecipient(recipientId)
+    fun observeWithCurrentRecipient(recipientId: String): LiveData<List<ChatMessage>>
+            = chatMessagesDao.loadMessagesByPeer(recipientId)
 }
