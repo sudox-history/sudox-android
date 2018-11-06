a0ca0497554c9eb02652284a4c42dd18ea17e610
TheMax <maksim182003mit@gmail.com>
2018-11-06 19:16:08 +0300

    Attempt to fix crash with database.

--- app/src/main/java/com/sudox/android/data/database/SudoxDatabase.kt
+++ app/src/main/java/com/sudox/android/data/database/SudoxDatabase.kt
@@ -7,7 +7,7 @@ import com.sudox.android.data.database.dao.ChatMessagesDao
 import com.sudox.android.data.database.model.User
 import com.sudox.android.data.database.model.ChatMessage
 
-@Database(entities = [User::class, ChatMessage::class], version = 1, exportSchema = false)
+@Database(entities = [User::class, ChatMessage::class], version = 2, exportSchema = false)
 abstract class SudoxDatabase : RoomDatabase() {
     abstract fun contactsDao(): ContactsDao
     abstract fun messagesDao(): ChatMessagesDao
