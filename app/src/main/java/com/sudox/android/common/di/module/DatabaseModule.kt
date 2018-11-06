d711a0ca00637eb4fa4712dc6f777fbe2ffc8ca5
TheMax <maksim182003mit@gmail.com>
2018-11-06 19:05:58 +0300

    Attempt to fix crash with database.

--- app/src/main/java/com/sudox/android/common/di/module/DatabaseModule.kt
+++ app/src/main/java/com/sudox/android/common/di/module/DatabaseModule.kt
@@ -14,6 +14,7 @@ class DatabaseModule {
     @Provides
     fun providesSudoxDatabase(context: Context) = Room
             .databaseBuilder(context, SudoxDatabase::class.java, "sudox-database")
+            .fallbackToDestructiveMigration()
             .build()
 
     @Singleton
