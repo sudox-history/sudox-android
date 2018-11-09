634f74e92f1e81e6100cf0f8679243abc9481e73
anton-pc <kerjen01@gmail.com>
2018-11-09 15:04:00 +0300

    [BASEMENT] Built basement for profile

---
+++ app/src/main/java/com/sudox/android/ui/main/profile/ProfileFragment.kt
@@ -0,0 +1,22 @@
+package com.sudox.android.ui.main.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sudox.android.ui.main.common.BaseReconnectFragment
+
+class ProfileFragment: BaseReconnectFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun showConnectionStatus(isConnect: Boolean) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ app/src/main/java/com/sudox/android/ui/main/profile/ProfileViewModel.kt
@@ -0,0 +1,4 @@
+package com.sudox.android.ui.main.profile
+
+class ProfileViewModel {
+}
