938f6ba942d42b0b83a463fb1f3a7596debf40b2
TheMax <maksim182003mit@gmail.com>
2018-11-12 19:04:56 +0300

    New toolbar.

---
+++ app/src/main/java/com/sudox/design/helpers/FontsHelper.kt
@@ -0,0 +1,12 @@
+package com.sudox.design.helpers
+
+import android.graphics.Typeface
+
+class FontsHelper {
+
+    companion object {
+
+        @JvmStatic
+        val SANS_SERIF_LIGHT by lazy { Typeface.create("sans-serif-light", Typeface.NORMAL) }
+    }
+}
--- app/src/main/java/com/sudox/design/navigation/toolbar/SudoxToolbar.kt
+++ app/src/main/java/com/sudox/design/navigation/toolbar/SudoxToolbar.kt
@@ -1,12 +1,125 @@
 package com.sudox.design.navigation.toolbar
 
 import android.content.Context
+import android.support.v7.widget.ActionMenuView
 import android.support.v7.widget.Toolbar
 import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.Gravity
+import android.widget.TextView
+import com.sudox.android.R
+import com.sudox.design.helpers.FontsHelper.Companion.SANS_SERIF_LIGHT
 
 class SudoxToolbar : Toolbar {
+
+    private var titleTextView: TextView? = null
+    private var actionMenuView: ActionMenuView? = null
 
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    init {
+        resetInsets()
+
+        // Configuring toolbar
+        configureMenu()
+        configureBasic()
+    }
+
+    private fun configureBasic() {
+        setBackgroundResource(R.color.colorPrimary)
+
+        // Remove shadow
+        elevation = 0F
+    }
+
+    private fun configureMenu() {
+        Toolbar::class.java
+                .getDeclaredMethod("ensureMenuView")
+                .apply { isAccessible = true }
+                .invoke(this)
+
+        // Cache menu for better performance
+        actionMenuView = Toolbar::class.java
+                .getDeclaredField("mMenuView")
+                .apply { isAccessible = true }
+                .get(this) as ActionMenuView
+    }
+
+    private fun configurePaddings() {
+        val menuItemsCount = actionMenuView?.childCount ?: 0
+        val initialEndPadding = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 25F, resources.displayMetrics)
+        val startPadding = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 25F, resources.displayMetrics).toInt()
+        val endPadding = calculateEndPadding(menuItemsCount, initialEndPadding.toInt())
+
+        setPaddingRelative(startPadding, 0, endPadding, 0)
+    }
+
+    private fun calculateEndPadding(menuItemsCount: Int, initialEndPadding: Int): Int {
+        if (menuItemsCount > 0) {
+            val lastItem = actionMenuView!!.getChildAt(menuItemsCount - 1)
+            val lastItemEndPadding = if (lastItem.paddingEnd > 0) lastItem.paddingEnd else lastItem.paddingRight
+
+            // Recalculate end padding
+            return initialEndPadding - lastItemEndPadding
+        }
+
+        return initialEndPadding
+    }
+
+    override fun setTitle(title: CharSequence) {
+        super.setTitle(title)
+
+        // Get title text view
+        if (titleTextView == null) {
+            titleTextView = Toolbar::class.java
+                    .getDeclaredField("mTitleTextView")
+                    .apply { isAccessible = true }
+                    .get(this) as TextView
+        }
+
+        titleTextView?.includeFontPadding = false
+        titleTextView?.typeface = SANS_SERIF_LIGHT
+        titleTextView?.setPadding(0, 0, 0, 0)
+        titleTextView?.setPaddingRelative(0, 0, 0, 0)
+        titleTextView?.setTextSize(TypedValue.COMPLEX_UNIT_SP, 19F)
+    }
+
+    private fun resetInsets() {
+        setContentInsetsAbsolute(0, 0)
+        setContentInsetsRelative(0, 0)
+
+        Toolbar::class.java
+                .getDeclaredField("mContentInsetStartWithNavigation")
+                .apply { isAccessible = true }
+                .set(this, 0)
+
+        Toolbar::class.java
+                .getDeclaredField("mContentInsetEndWithActions")
+                .apply { isAccessible = true }
+                .set(this, 0)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        // Configure paddings
+        configurePaddings()
+
+        // Calculate new sizes
+        val width = measuredWidthAndState
+        val height = MeasureSpec.makeMeasureSpec(resources.getDimension(R.dimen.toolbar_height).toInt(), MeasureSpec.EXACTLY)
+
+        // Set new size
+        setMeasuredDimension(width, height)
+    }
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        super.onLayout(changed, l, t, r, b)
+
+        // Align action menu to vertical center
+        actionMenuView?.layoutParams = (actionMenuView?.layoutParams as Toolbar.LayoutParams)
+                .apply { gravity = Gravity.CENTER_VERTICAL }
+    }
 }
