3c100300a4b61dfc2a5287e9779f8e7b82fbc26a
TheMax <maksim182003mit@gmail.com>
2019-01-20 13:37:28 +0300

    Fixed SA-67 (https://sudox.myjetbrains.com/youtrack/issue/SA-67)

--- app/src/main/java/com/sudox/android/common/helpers/RegexHelper.kt
+++ app/src/main/java/com/sudox/android/common/helpers/RegexHelper.kt
@@ -12,9 +12,9 @@ val NUMBER_REGEX = "^[0-9]+".toRegex()
 val NAME_REGEX = "^[-a-zA-Zа-яА-Я]{1,20}[ ]?[-a-zA-Zа-яА-Я]{1,20}$".toRegex()
 val NICKNAME_REGEX = "^[-a-zA-Z0-9.]{1,20}$".toRegex()
 val WHITESPACES_REMOVE_REGEX = " +".toRegex()
-val NEW_LINE_ON_START_REMOVE_REGEX = " .n".toRegex(RegexOption.DOT_MATCHES_ALL)
-val NEW_LINE_ON_END_REMOVE_REGEX = ".n ".toRegex(RegexOption.DOT_MATCHES_ALL)
-val NEW_LINE_MULTIPLE_REMOVE_REGEX = "(.n){3,}".toRegex(RegexOption.DOT_MATCHES_ALL)
+val NEW_LINE_ON_START_REMOVE_REGEX = " \n".toRegex(RegexOption.DOT_MATCHES_ALL)
+val NEW_LINE_ON_END_REMOVE_REGEX = "\n ".toRegex(RegexOption.DOT_MATCHES_ALL)
+val NEW_LINE_MULTIPLE_REMOVE_REGEX = "(\n){3,}".toRegex(RegexOption.DOT_MATCHES_ALL)
 
 fun formatMessageText(message: String): String {
     // TODO. In future need be move to the AbstractMessagesRepository
