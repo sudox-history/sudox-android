01c0e3d1ea6e949522a7f94dcc3db6209733b159
TheMax <maksim182003mit@gmail.com>
2019-01-27 22:52:58 +0300

    Patch #2 for SA-44 fix (https://sudox.myjetbrains.com/youtrack/issue/SA-44)

--- app/src/main/java/com/sudox/design/layouts/MessageLinearLayout.kt
+++ app/src/main/java/com/sudox/design/layouts/MessageLinearLayout.kt
@@ -25,29 +25,31 @@ class MessageLinearLayout : LinearLayout {
             val neededSpace = dialogMessageSendTime.measuredWidth + TIME_MARGIN
 
             if (freeSpace >= neededSpace) {
-                dialogMessageSendTime.translationY = (-dialogMessageSendTime.measuredHeight).toFloat()
+                dialogMessageSendTime.translationY = (-dialogMessageSendTime.measuredHeight / 1.25F)
 
                 // Remove parasitic bottom padding
-                setMeasuredDimension(measuredWidth, measuredHeight - dialogMessageSendTime.measuredHeight)
+                setMeasuredDimension(measuredWidth, (measuredHeight - (dialogMessageSendTime.measuredHeight)).toInt())
             } else if (dialogMessageText.layout.lineCount == 1 && measuredWidth + neededSpace <= MAX_WIDTH) {
-                dialogMessageSendTime.translationY = (-dialogMessageSendTime.measuredHeight).toFloat()
+                dialogMessageSendTime.translationY = (-dialogMessageSendTime.measuredHeight / 1.25F)
 
                 // Remove parasitic bottom padding
-                setMeasuredDimension(measuredWidth + neededSpace, measuredHeight - dialogMessageSendTime.measuredHeight)
+                setMeasuredDimension(measuredWidth + neededSpace, (measuredHeight - (dialogMessageSendTime.measuredHeight)).toInt())
             } else {
                 if (measuredWidth < neededSpace) {
-                    dialogMessageSendTime.translationY = (-dialogMessageSendTime.measuredHeight).toFloat()
+                    dialogMessageSendTime.translationY = (-dialogMessageSendTime.measuredHeight / 1.25F)
 
                     // Remove parasitic bottom padding and add needed width
-                    setMeasuredDimension(measuredWidth + neededSpace, measuredHeight - dialogMessageSendTime.measuredHeight)
+                    setMeasuredDimension(measuredWidth + neededSpace, (measuredHeight - (dialogMessageSendTime.measuredHeight)))
                 } else {
                     if (lastLineWidth + neededSpace <= MAX_WIDTH) {
-                        dialogMessageSendTime.translationY = (-dialogMessageSendTime.measuredHeight).toFloat()
+                        dialogMessageSendTime.translationY = (-dialogMessageSendTime.measuredHeight / 1.25F)
 
                         // Remove parasitic bottom padding and add needed width
-                        setMeasuredDimension(measuredWidth + neededSpace, measuredHeight - dialogMessageSendTime.measuredHeight)
+                        setMeasuredDimension(measuredWidth + neededSpace, (measuredHeight - (dialogMessageSendTime.measuredHeight)))
                     } else {
                         dialogMessageSendTime.translationY = 0F
+
+                        setMeasuredDimension(measuredWidth, ((measuredHeight - (dialogMessageSendTime.measuredHeight  * 0.20F)).toInt()))
                     }
                 }
             }
