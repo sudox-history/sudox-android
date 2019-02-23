cc89f4d28e2df8892623b023657f9947978511ca
TheMax <maksim182003mit@gmail.com>
2019-02-23 23:29:34 +0300

    Implemented SA-141 (https://sudox.myjetbrains.com/youtrack/issue/SA-141)

--- app/src/main/java/com/sudox/design/helpers/DesignHelper.kt
+++ app/src/main/java/com/sudox/design/helpers/DesignHelper.kt
@@ -10,9 +10,6 @@ import android.view.ContextMenu
 import android.view.MenuItem
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.ImageView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 
 @Suppress("DEPRECATION")
 fun formatHtml(string: String): Spanned {
@@ -33,31 +30,6 @@ fun hideKeyboard(context: Context, view: View?) {
     inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 }
 
-fun drawAvatar(text: String, firstColor: String, secondColor: String, textColor: Int = Color.WHITE): Bitmap {
-    val bitmap = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888)
-    val canvas = Canvas(bitmap)
-    val formattedText = text.toUpperCase()
-    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
-
-    // Draw gradient
-    paint.shader = LinearGradient(0F, 0F, 200F, 200F, Color.parseColor(firstColor), Color.parseColor(secondColor), Shader.TileMode.REPEAT)
-
-    // Draw circle
-    canvas.drawRect(0F, 0F, 200F, 200F, paint)
-
-    // Text bounds
-    val textRect = Rect()
-
-    // Draw text
-    paint.shader = null
-    paint.color = textColor
-    paint.textSize = 60F
-    paint.getTextBounds(formattedText, 0, formattedText.length, textRect)
-    canvas.drawText(formattedText, canvas.width / 2 - textRect.exactCenterX(), canvas.height / 2 - textRect.exactCenterY(), paint)
-
-    return bitmap
-}
-
 fun ContextMenu.setOnItemClickListener(callback: (MenuItem) -> (Boolean)) {
     val size = size()
 
@@ -65,13 +37,6 @@ fun ContextMenu.setOnItemClickListener(callback: (MenuItem) -> (Boolean)) {
     for (i in 0 until size) {
         getItem(i).setOnMenuItemClickListener { callback(it) }
     }
-}
-
-fun drawCircleBitmap(context: Context, bitmap: Bitmap, view: ImageView) {
-    Glide.with(context)
-            .load(bitmap)
-            .apply(RequestOptions.circleCropTransform())
-            .into(view)
 }
 
 /**
