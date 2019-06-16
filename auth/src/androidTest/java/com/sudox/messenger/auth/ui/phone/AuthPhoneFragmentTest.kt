4fef7db2fd288c644f4f9dc7ad1a3dacf6ab8ad9
Maxim Kotlinovsky <maksim182003mit@gmail.com>
2019-06-17 02:26:14 +0300

    Removed unused code.

--- auth/src/androidTest/java/com/sudox/messenger/auth/ui/phone/AuthPhoneFragmentTest.kt
+++ auth/src/androidTest/java/com/sudox/messenger/auth/ui/phone/AuthPhoneFragmentTest.kt
@@ -1,9 +1,6 @@
 package com.sudox.messenger.auth.ui.phone
 
-import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.sudox.api.auth.AuthErrorCodes
-import kotlinx.android.synthetic.main.fragment_auth_phone.*
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -13,11 +10,6 @@ class AuthPhoneFragmentTest : Assert() {
 
     @Test
     fun testErrorsHandling() {
-//        val fragment = launchFragmentInContainer<AuthPhoneFragment>()
-//
-//        fragment.onFragment {
-//            it.authPhoneViewModel!!.errorsLiveData.postValue(AuthErrorCodes.INVALID_PARAMETERS)
-//            assertEquals(it.authPhoneEditTextLayout.resetErrorText())
-//        }
+
     }
 }
