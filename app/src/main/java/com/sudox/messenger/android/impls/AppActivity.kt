acbdc2baa1a57caf70ca6b0db0d3ad154d033e1a
Maxim Mityushkin <maksim182003mit@gmail.com>
2019-10-02 22:07:42 +0300

    Connected fonts & fixed button background color.

--- app/src/main/java/com/sudox/messenger/android/impls/AppActivity.kt
+++ app/src/main/java/com/sudox/messenger/android/impls/AppActivity.kt
@@ -2,10 +2,12 @@ package com.sudox.messenger.android.impls
 
 import android.app.Activity
 import android.os.Bundle
+import com.sudox.messenger.android.R
 
 class AppActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app)
     }
 }
