34fb4f0690e9e00f2428ef9c71a0a60cfdd0aade
Maxim Mityushkin <maksim182003mit@gmail.com>
2019-10-02 21:06:53 +0300

    Implemented splash screen layout.

--- app/src/main/java/com/sudox/messenger/android/impls/AppActivity.kt
+++ app/src/main/java/com/sudox/messenger/android/impls/AppActivity.kt
@@ -2,13 +2,10 @@ package com.sudox.messenger.android.impls
 
 import android.app.Activity
 import android.os.Bundle
-import com.sudox.design.drawables.GradientBackgroundDrawable
 
 class AppActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        window.setBackgroundDrawable(GradientBackgroundDrawable(this))
-
         super.onCreate(savedInstanceState)
     }
 }
