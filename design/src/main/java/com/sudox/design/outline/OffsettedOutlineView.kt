ea597ee5b152e5db511b95aad1ef62af1f5775e6
Maxim Mityushkin <maksim182003mit@gmail.com>
2019-10-14 23:01:17 +0300

    [MAIN] Removed unused outline hack.

--- design/src/main/java/com/sudox/design/outline/OffsettedOutlineView.kt
+++
@@ -1,8 +0,0 @@
-package com.sudox.design.outline
-
-interface OffsettedOutlineView {
-    fun getTopOutlineOffset(): Int
-    fun getBottomOutlineOffset(): Int
-    fun getLeftOutlineOffset(): Int
-    fun getRightOutlineOffset(): Int
-}
