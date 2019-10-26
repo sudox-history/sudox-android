d0dc71eb900fb5ec115887d91bd5d24a10339000
Maxim Mityushkin <maksim182003mit@gmail.com>
2019-10-26 23:29:21 +0300

    [TASK-32] Implemented & tested NicknameEditText.

---
+++ design/src/main/java/com/sudox/design/nicknameEditText/NicknameEditText.kt
@@ -0,0 +1,61 @@
+package com.sudox.design.nicknameEditText
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Rect
+import android.text.style.ForegroundColorSpan
+import android.util.AttributeSet
+import android.view.inputmethod.EditorInfo
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.res.getColorOrThrow
+import androidx.core.content.res.use
+import com.sudox.design.R
+
+internal const val TAG_SPLITTER = '#'
+
+class NicknameEditText : AppCompatEditText {
+
+    private var tagColor = 0
+
+    internal var tag: String? = null
+    internal var tagBounds = Rect()
+    internal var tagColorSpannable: ForegroundColorSpan? = null
+
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, R.attr.nicknameEditTextStyle)
+
+    @SuppressLint("Recycle")
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        context.obtainStyledAttributes(attrs, R.styleable.NicknameEditText, defStyleAttr, 0).use {
+            tagColor = it.getColorOrThrow(R.styleable.NicknameEditText_tagTextColor)
+        }
+    }
+
+    init {
+        isSingleLine = true
+        imeOptions = EditorInfo.IME_ACTION_DONE
+        maxLines = 1
+
+        addTextChangedListener(NicknameTextWatcher(this))
+    }
+
+    fun setTag(tag: String?) {
+        this.tag = "${TAG_SPLITTER}$tag"
+        this.tagColorSpannable = ForegroundColorSpan(tagColor)
+
+        paint.getTextBounds(this.tag!!, 0, this.tag!!.length, tagBounds)
+    }
+
+    fun scrollToEnd() {
+        // Spannables not calculating
+        val totalTextWidth = paint.measureText(text.toString() + tag).toInt()
+        val width = right - left
+
+        if (totalTextWidth < width) {
+            return
+        }
+
+        val needScroll = totalTextWidth - width
+        scrollTo(needScroll, scrollY)
+    }
+}
---
+++ design/src/test/java/com/sudox/design/nicknameEditText/NicknameEditTextTest.kt
@@ -0,0 +1,92 @@
+package com.sudox.design.nicknameEditText
+
+import android.app.Activity
+import android.os.Bundle
+import com.sudox.design.DesignTestRunner
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.android.controller.ActivityController
+
+@RunWith(DesignTestRunner::class)
+class NicknameEditTextTest : Assert() {
+
+    private var nicknameEditText: NicknameEditText? = null
+    private var activityController: ActivityController<Activity>? = null
+
+    @Before
+    fun setUp() {
+        createActivity()
+    }
+
+    private fun createActivity() {
+        var state: Bundle? = null
+
+        activityController?.let {
+            state = Bundle()
+
+            it.saveInstanceState(state)
+            it.pause()
+            it.stop()
+            it.destroy()
+        }
+
+        activityController = Robolectric
+                .buildActivity(Activity::class.java)
+                .create()
+                .start()
+
+        activityController!!.get().apply {
+            nicknameEditText = NicknameEditText(this).apply {
+                id = Int.MAX_VALUE
+            }
+
+            setContentView(nicknameEditText)
+        }
+
+        state?.let {
+            activityController!!.restoreInstanceState(state)
+        }
+
+        activityController!!
+                .resume()
+                .visible()
+    }
+
+    @Test
+    fun testThatTagInserted() {
+        nicknameEditText!!.setTag("4877")
+        nicknameEditText!!.setText("nickname")
+
+        assertEquals("nickname#4877", nicknameEditText!!.text.toString())
+    }
+
+    @Test
+    fun testThatTagRemoved() {
+        nicknameEditText!!.setTag("4877")
+        nicknameEditText!!.setText("nickname")
+        nicknameEditText!!.setText("")
+
+        assertEquals("", nicknameEditText!!.text.toString())
+    }
+
+    @Test
+    fun testThatTagFixed() {
+        nicknameEditText!!.setTag("4877")
+        nicknameEditText!!.setText("nickname#4877")
+        nicknameEditText!!.setText("nickname#4877-invalid-data")
+
+        assertEquals("nickname#4877", nicknameEditText!!.text.toString())
+    }
+
+    @Test
+    fun testThatTagFixedBeforeSplitterRemoved() {
+        nicknameEditText!!.setTag("4877")
+        nicknameEditText!!.setText("nickname#4877")
+        nicknameEditText!!.setText("nickname4877")
+
+        assertEquals("nickname#4877", nicknameEditText!!.text.toString())
+    }
+}
