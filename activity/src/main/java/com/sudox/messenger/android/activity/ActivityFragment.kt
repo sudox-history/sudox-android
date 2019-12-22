66c38017ff118045b74868871da4dfc0d6bd956e
TheMax <maksim182003mit@gmail.com>
2019-12-22 14:44:22 +0300

    [TASK-62] Fixed view indicator radius.

--- activity/src/main/java/com/sudox/messenger/android/activity/ActivityFragment.kt
+++ activity/src/main/java/com/sudox/messenger/android/activity/ActivityFragment.kt
@@ -10,6 +10,8 @@ import com.sudox.messenger.android.core.CoreActivity
 import com.sudox.messenger.android.core.CoreFragment
 import com.sudox.messenger.android.core.viewPager.ViewPagerFragment
 import kotlinx.android.synthetic.main.fragment_activity.momentView
+import kotlinx.android.synthetic.main.fragment_activity.momentView1
+import kotlinx.android.synthetic.main.fragment_activity.momentView2
 
 class ActivityFragment : CoreFragment(), ViewPagerFragment, ApplicationBarListener {
 
@@ -22,6 +24,16 @@ class ActivityFragment : CoreFragment(), ViewPagerFragment, ApplicationBarListen
         momentView.setUserPhoto(resources.getDrawable(R.drawable.drawable_photo_3, context!!.theme))
         momentView.setCreatedByMe(true)
         momentView.setViewed(false)
+
+        momentView1.setUserName("Yaroslav")
+        momentView1.setUserPhoto(resources.getDrawable(R.drawable.drawable_photo_2, context!!.theme))
+        momentView1.setCreatedByMe(false)
+        momentView1.setViewed(false)
+
+        momentView2.setUserName("Anton")
+        momentView2.setUserPhoto(resources.getDrawable(R.drawable.drawable_photo_1, context!!.theme))
+        momentView2.setCreatedByMe(false)
+        momentView2.setViewed(true)
     }
 
     override fun onButtonClicked(tag: Int) {
