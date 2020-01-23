a9af6b2af9bc5a3f4e4f28f5cb020d706b4eb479
Andrew <andrewkir2001@mail.ru>
2020-01-23 22:57:48 +0300

    add styles attributes

--- messages/src/main/java/com/sudox/messenger/android/messages/views/DialogItemView.kt
+++ messages/src/main/java/com/sudox/messenger/android/messages/views/DialogItemView.kt
@@ -11,43 +11,23 @@ import android.util.AttributeSet
 import android.view.Gravity
 import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.res.getResourceIdOrThrow
 import androidx.core.content.res.use
+import androidx.core.widget.TextViewCompat.setTextAppearance
 import com.sudox.design.circleImageView.CircleImageView
 import com.sudox.messenger.android.messages.R
 
 class DialogItemView : ViewGroup{
-        private var myMomentNameText: String? = null
-        private var nameTextMarginRelativeToPhoto = 0
-
-        private var viewIndicatorRadius = 0
-        private var viewIndicatorActiveColor = 0
-        private var viewIndicatorDefaultColor = 0
-        private var viewIndicatorDefaultStrokeWidth = 0
-        private var viewIndicatorActiveStrokeWidth = 0
-        private var viewIndicatorPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-            style = Paint.Style.STROKE
-        }
-
-        private var addIndicatorCropPaint = Paint(Paint.ANTI_ALIAS_FLAG)
-        private var addIndicatorStrokePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-            style = Paint.Style.STROKE
-        }
-
-        private var addIndicatorIcon: Drawable? = null
-        private var addIndicatorCropRadius = 0
-        private var addIndicatorRadius = 0
 
         private var nameView = AppCompatTextView(context).apply { addView(this) }
-        private var photoView = CircleImageView(context).apply { addView(this) }
-        private var isViewed = false
 
         constructor(context: Context) : this(context, null)
-        constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, R.attr.dialogItemView)
+        constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, R.attr.dialogItemViewStyle)
 
         @SuppressLint("Recycle")
         constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
             context.obtainStyledAttributes(attrs, R.styleable.DialogItemView, defStyleAttr, 0).use {
-
+                setTextAppearance(nameView, it.getResourceIdOrThrow(R.styleable.DialogItemView_dialogNameTextAppearance))
             }
 
             nameView.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
@@ -70,40 +50,5 @@ class DialogItemView : ViewGroup{
             val nameRightBorder = nameLeftBorder + nameView.measuredWidth
 
             nameView.layout(nameLeftBorder, nameTopBorder, nameRightBorder, nameBottomBorder)
-        }
-
-        override fun dispatchDraw(canvas: Canvas) {
-            super.dispatchDraw(canvas)
-        }
-
-        fun setCreatedByMe(createdByMe: Boolean) {
-
-        }
-
-        fun setViewed(viewed: Boolean) {
-            this.isViewed = viewed
-
-            if (!viewed) {
-                viewIndicatorPaint.strokeWidth = viewIndicatorActiveStrokeWidth.toFloat()
-                viewIndicatorPaint.color = viewIndicatorActiveColor
-            } else {
-                viewIndicatorPaint.strokeWidth = viewIndicatorDefaultStrokeWidth.toFloat()
-                viewIndicatorPaint.color = viewIndicatorDefaultColor
-            }
-
-            requestLayout()
-            invalidate()
-        }
-
-        fun setUserPhoto(bitmap: Bitmap?) {
-            photoView.setImageBitmap(bitmap)
-        }
-
-        fun setUserPhoto(drawable: Drawable?) {
-            photoView.setImageDrawable(drawable)
-        }
-
-        fun setUserName(name: String?) {
-            nameView.text = name
         }
 }
