ee7cec701ce273185f3a9e5bbe52ce570e2d7d15
Kotlinovsky <kotlinovsky@gmail.com>
2020-01-27 01:28:57 +0300

    [MASTER] Fixed rectangle image showing in CircleImageView.

--- design/src/main/java/com/sudox/design/circleImageView/CircleImageView.kt
+++ design/src/main/java/com/sudox/design/circleImageView/CircleImageView.kt
@@ -23,19 +23,21 @@ class CircleImageView : AppCompatImageView {
         xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_OUT)
     }
 
-    constructor(context: Context) : super(context)
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        scaleType = ScaleType.CENTER_CROP
+    }
 
     override fun onDraw(canvas: Canvas) {
-        val imageLayer = canvas.saveLayer(0f, 0f, width.toFloat(), height.toFloat(), simplePaint)
+        val imageLayer = canvas.saveLayer(0F, 0F, width.toFloat(), height.toFloat(), simplePaint)
 
         super.onDraw(canvas)
 
-        val mergeLayer = canvas.saveLayer(0f, 0f, width.toFloat(), height.toFloat(), mergePaint)
+        val mergeLayer = canvas.saveLayer(0F, 0F, width.toFloat(), height.toFloat(), mergePaint)
 
         canvas.drawColor(Color.GREEN)
-        canvas.drawCircle(width / 2f, height / 2f, min(width, height) / 2f, circlePaint)
+        canvas.drawCircle(width / 2F, height / 2F, min(width, height) / 2F, circlePaint)
         canvas.restoreToCount(mergeLayer)
         canvas.restoreToCount(imageLayer)
     }
