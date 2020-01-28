ca09730c90e1a746123334c2715c742e962f9a64
Andrew <andrewkir2001@mail.ru>
2020-01-28 21:32:40 +0300

    [TASK-42 add canvas counter view]

--- messages/src/main/java/com/sudox/messenger/android/messages/MessagesFragment.kt
+++ messages/src/main/java/com/sudox/messenger/android/messages/MessagesFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import com.sudox.design.applicationBar.ApplicationBarListener
 import com.sudox.messenger.android.core.CoreActivity
 import com.sudox.messenger.android.core.CoreFragment
@@ -19,6 +20,22 @@ class MessagesFragment : CoreFragment(), ViewPagerFragment, ApplicationBarListen
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        testDialogItem1.setContentText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus enim ligula")
+        testDialogItem1.setIsNewMessage(false)
+        testDialogItem1.setCountMessages(100)
+        testDialogItem1.setLastDate("21 дек.")
+        testDialogItem1.setDialogName("Антон")
+        testDialogItem1.setDialogImage(getDrawable(context!!, R.drawable.drawable_photo_1)!!)
+
+
+        testDialogItem2.setContentText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus enim ligula")
+        testDialogItem2.setIsNewMessage(true)
+        testDialogItem2.setCountMessages(202)
+        testDialogItem2.setLastDate("пн")
+        testDialogItem2.setDialogName("ЯроСЛАУ")
+        testDialogItem2.setDialogImage(getDrawable(context!!, R.drawable.drawable_photo_2)!!)
+
     }
 
     override fun getPageTitle(context: Context): CharSequence? {
--- messages/src/main/java/com/sudox/messenger/android/messages/views/DialogItemView.kt
+++ messages/src/main/java/com/sudox/messenger/android/messages/views/DialogItemView.kt
@@ -2,6 +2,10 @@ package com.sudox.messenger.android.messages.views
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
 import android.graphics.drawable.Drawable
 import android.text.TextUtils
 import android.util.AttributeSet
@@ -14,13 +18,16 @@ import androidx.core.content.res.*
 import androidx.core.widget.TextViewCompat.setTextAppearance
 import com.sudox.design.circleImageView.CircleImageView
 import com.sudox.messenger.android.messages.R
+import kotlin.math.max
 
-class DialogItemView : ViewGroup{
+
+class DialogItemView : ViewGroup {
 
     private val nameView = AppCompatTextView(context).apply { addView(this) }
     private val contentTextView = AppCompatTextView(context).apply { addView(this) }
     private val photoView = CircleImageView(context).apply { addView(this) }
     private val dateView = AppCompatTextView(context).apply { addView(this) }
+    private val countMessagesView = AppCompatTextView(context).apply { addView(this) }
 
     private var imageHeight = 0
     private var imageWidth = 0
@@ -30,14 +37,25 @@ class DialogItemView : ViewGroup{
     private var messageStatusIcon: Drawable? = null
 
     private var innerImageToTextMargin = 0
-    private var innerDialogNameToTopMargin= 0
-    private var innerDialogNameToContentMargin= 0
-    private var innerDateToTopMargin= 0
-    private var innerDateToCountMargin= 0
+    private var innerDialogNameToTopMargin = 0
+    private var innerDialogNameToContentMargin = 0
+    private var innerDateToTopMargin = 0
+    private var innerDateToCountMargin = 0
     private var innerMessageStatusMargin = 0
-    private var innerContentToRightBorderMargin = 0
+    private var innerContentToRightViewMargin = 0
 
     private var isNewMessage = true
+
+    private val countMessagesPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private var paintCountColor = 0
+    private var countMessagesRect = RectF()
+    private var countMessagesHeight = 0
+    private var countMessagesRadius = 0
+    private var innerCounterHorizontalMargin = 0
+    private var innerCounterVerticalMargin = 0
+
+    private var dialogContentTextAppearance = 0
+    private var dialogContentNewTextAppearance = 0
 
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, R.attr.dialogItemViewStyle)
@@ -47,14 +65,12 @@ class DialogItemView : ViewGroup{
         context.obtainStyledAttributes(attrs, R.styleable.DialogItemView, defStyleAttr, 0).use {
             setTextAppearance(nameView, it.getResourceIdOrThrow(R.styleable.DialogItemView_dialogNameTextAppearance))
             setTextAppearance(dateView, it.getResourceIdOrThrow(R.styleable.DialogItemView_dialogDateTextAppearance))
-            if(!isNewMessage) {
-                setTextAppearance(contentTextView, it.getResourceIdOrThrow(R.styleable.DialogItemView_dialogContentTextAppearance))
-            } else {
-                setTextAppearance(contentTextView, it.getResourceIdOrThrow(R.styleable.DialogItemView_dialogNewContentTextAppearance))
-            }
+            dialogContentTextAppearance = it.getResourceIdOrThrow(R.styleable.DialogItemView_dialogContentTextAppearance)
+            dialogContentNewTextAppearance = it.getResourceIdOrThrow(R.styleable.DialogItemView_dialogNewContentTextAppearance)
+            setTextAppearance(countMessagesView, it.getResourceIdOrThrow(R.styleable.DialogItemView_dialogMessageCountTextAppearance))
 
             imageHeight = it.getDimensionPixelSizeOrThrow(R.styleable.DialogItemView_imageHeight)
-            imageWidth =  it.getDimensionPixelSizeOrThrow(R.styleable.DialogItemView_imageWidth)
+            imageWidth = it.getDimensionPixelSizeOrThrow(R.styleable.DialogItemView_imageWidth)
 
             imageActiveColor = it.getColorOrThrow(R.styleable.DialogItemView_imageActiveColor)
             imageActiveRadius = it.getDimensionPixelSizeOrThrow(R.styleable.DialogItemView_imageActiveRadius)
@@ -67,37 +83,17 @@ class DialogItemView : ViewGroup{
             innerDateToTopMargin = it.getDimensionPixelSizeOrThrow(R.styleable.DialogItemView_innerDateToTopMargin)
             innerDateToCountMargin = it.getDimensionPixelSizeOrThrow(R.styleable.DialogItemView_innerDateToCountMargin)
             innerMessageStatusMargin = it.getDimensionPixelSizeOrThrow(R.styleable.DialogItemView_innerMessageStatusMargin)
-            innerContentToRightBorderMargin = it.getDimensionPixelSizeOrThrow(R.styleable.DialogItemView_innerContentToRightBorderMargin)
+            innerContentToRightViewMargin = it.getDimensionPixelSizeOrThrow(R.styleable.DialogItemView_innerContentToRightViewMargin)
+            innerCounterHorizontalMargin = it.getDimensionPixelSizeOrThrow(R.styleable.DialogItemView_countMessagesInnerHorizontalMargin)
+            innerCounterVerticalMargin = it.getDimensionPixelSizeOrThrow(R.styleable.DialogItemView_countMessagesInnerVerticalMargin)
+
+            countMessagesHeight = it.getDimensionPixelSizeOrThrow(R.styleable.DialogItemView_countMessagesHeight)
+            countMessagesRadius = it.getDimensionPixelSizeOrThrow(R.styleable.DialogItemView_countMessagesRadius)
+
+            paintCountColor = it.getColorOrThrow(R.styleable.DialogItemView_messageCountColor)
         }
 
-        //dialog name view settings
-        nameView.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
-        nameView.gravity = Gravity.LEFT
-        nameView.ellipsize = TextUtils.TruncateAt.END
-        nameView.isSingleLine = true
-        nameView.maxLines = 1
-        nameView.text = "Антон"
-
-        //dialog content view settings
-        contentTextView.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
-        contentTextView.gravity = Gravity.LEFT
-        contentTextView.ellipsize = TextUtils.TruncateAt.END
-        if(!isNewMessage) {
-            contentTextView.maxLines = 1
-        } else {
-            contentTextView.maxLines = 2
-        }
-        contentTextView.text = "aLorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus enim ligula, tristiq"
-
-
-        //dialog image settings
-        photoView.layoutParams = LayoutParams(imageWidth, imageHeight)
-        photoView.scaleType = ImageView.ScaleType.CENTER_CROP
-        photoView.setImageDrawable(getDrawable(context,R.drawable.drawable_photo_1)) //TODO remove later
-
-        //date view settings
-        dateView.layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
-        dateView.text = "21 янв."
+        textViewSettings()
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -106,17 +102,22 @@ class DialogItemView : ViewGroup{
         val availableWidth = MeasureSpec.getSize(widthMeasureSpec)
         val availableHeight = MeasureSpec.getSize(heightMeasureSpec)
 
+        measureChild(dateView, widthMeasureSpec, heightMeasureSpec)
         measureChild(nameView, widthMeasureSpec, heightMeasureSpec)
         measureChild(photoView, widthMeasureSpec, heightMeasureSpec)
-        measureChild(dateView, widthMeasureSpec, heightMeasureSpec)
-        //measureChild(contentTextView, widthMeasureSpec, heightMeasureSpec)
+        if (isNewMessage) {
+            measureChild(countMessagesView, widthMeasureSpec, heightMeasureSpec)
+        }
 
-        val contentTextWidth = availableWidth - imageWidth - innerImageToTextMargin - if(isNewMessage) innerContentToRightBorderMargin else innerContentToRightBorderMargin
+        val contentTextWidth = availableWidth - paddingLeft - imageWidth - innerImageToTextMargin  - innerContentToRightViewMargin - max(innerCounterHorizontalMargin * 2 + countMessagesView.measuredWidth, dateView.measuredWidth)- paddingRight
 
         measureChild(contentTextView, MeasureSpec.makeMeasureSpec(contentTextWidth, MeasureSpec.EXACTLY), heightMeasureSpec)
+        measureChild(nameView, MeasureSpec.makeMeasureSpec(contentTextWidth, MeasureSpec.EXACTLY), heightMeasureSpec)
 
+        val needHeight = paddingTop + nameView.measuredHeight + innerDialogNameToContentMargin + max(contentTextView.measuredHeight, photoView.measuredHeight) + paddingBottom
+        val needWidth = paddingLeft + photoView.measuredWidth + innerImageToTextMargin + contentTextWidth + innerContentToRightViewMargin + max(innerCounterHorizontalMargin * 2 + countMessagesView.measuredWidth, dateView.measuredWidth) + paddingRight
 
-        setMeasuredDimension(widthMeasureSpec,heightMeasureSpec)
+        setMeasuredDimension(needWidth, needHeight)
     }
 
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
@@ -139,15 +140,119 @@ class DialogItemView : ViewGroup{
         val contentRightBorder = contentLeftBorder + contentTextView.measuredWidth
         val contentBottomBorder = contentTopBorder + contentTextView.measuredHeight
 
-        dateView.layout(
-                rightBorder - dateView.measuredWidth,
-                paddingTop + innerDateToTopMargin,
-                rightBorder,
-                dateView.measuredHeight
-        )
+        val dateLeftBorder = rightBorder - dateView.measuredWidth
+        val dateTopBorder = paddingTop + innerDateToTopMargin
+        val dateRightBorder = rightBorder - paddingRight
+        val dateBottomBorder = dateTopBorder + dateView.measuredHeight
+
+        if(isNewMessage) {
+            val counterLeftBorder = rightBorder - countMessagesView.measuredWidth - innerCounterHorizontalMargin
+            val counterTopBorder = dateBottomBorder + innerDateToCountMargin + innerCounterVerticalMargin
+            val counterRightBorder = rightBorder - paddingRight - innerCounterHorizontalMargin
+            val counterBottomBorder = counterTopBorder + dateView.measuredHeight
+
+            countMessagesView.layout(counterLeftBorder, counterTopBorder, counterRightBorder, counterBottomBorder)
+        }
 
         photoView.layout(photoLeftBorder, photoTopBorder, photoRightBorder, photoBottomBorder)
         nameView.layout(dialogNameLeftBorder, dialogNameTopBorder, dialogNameRightBorder, dialogNameBottomBorder)
         contentTextView.layout(contentLeftBorder, contentTopBorder, contentRightBorder, contentBottomBorder)
+        dateView.layout(dateLeftBorder, dateTopBorder, dateRightBorder, dateBottomBorder)
+    }
+
+    override fun dispatchDraw(canvas: Canvas) {
+        if(isNewMessage && countMessagesView.text != "0") {
+            val leftCountBorder = width.toFloat() - paddingRight - innerCounterHorizontalMargin * 2 - countMessagesView.measuredWidth
+            val topCountBorder = paddingTop.toFloat() + innerDateToTopMargin + dateView.measuredHeight + innerDateToCountMargin
+            val rightCountBorder = width.toFloat() - paddingRight
+            val bottomCountBorder = topCountBorder + 2 * innerCounterVerticalMargin + countMessagesView.measuredHeight.toFloat()
+            countMessagesRect = RectF(leftCountBorder, topCountBorder, rightCountBorder, bottomCountBorder)
+            countMessagesPaint.color = paintCountColor
+
+            canvas.drawRoundRect(
+                    countMessagesRect,
+                    countMessagesRadius.toFloat(),
+                    countMessagesRadius.toFloat(),
+                    countMessagesPaint
+            )
+        }
+        super.dispatchDraw(canvas)
+    }
+
+
+    fun setContentText(content: String){
+        contentTextView.text = content
+    }
+
+    fun setIsNewMessage(isNew: Boolean){
+        this.isNewMessage = isNew
+        textViewSettings()
+
+        requestLayout()
+        invalidate()
+    }
+
+    fun setCountMessages(number: Int){
+        countMessagesView.text = number.toString()
+    }
+
+    fun setDialogImage(drawable: Drawable){
+        photoView.setImageDrawable(drawable)
+    }
+
+    fun setDialogImage(bitmap: Bitmap){
+        photoView.setImageBitmap(bitmap)
+    }
+
+    fun setDialogName(name: String){
+        nameView.text = name
+    }
+
+    fun setLastDate(date: String){
+        dateView.text = date
+    }
+
+    private fun textViewSettings(){
+        //text appearance
+        if(isNewMessage) {
+            setTextAppearance(contentTextView, dialogContentNewTextAppearance)
+        } else {
+            setTextAppearance(contentTextView, dialogContentTextAppearance)
+        }
+        //dialog name view settings
+        nameView.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+        nameView.gravity = Gravity.LEFT
+        nameView.ellipsize = TextUtils.TruncateAt.END
+        nameView.isSingleLine = true
+        nameView.maxLines = 1
+
+        //dialog content view settings
+        contentTextView.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+        contentTextView.gravity = Gravity.LEFT
+        contentTextView.ellipsize = TextUtils.TruncateAt.END
+        if (isNewMessage) {
+            contentTextView.maxLines = 2
+        } else {
+            contentTextView.maxLines = 1
+        }
+
+
+        //dialog image settings
+        photoView.layoutParams = LayoutParams(imageWidth, imageHeight)
+        photoView.scaleType = ImageView.ScaleType.CENTER_CROP
+
+        //date view settings
+        dateView.layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+        dateView.gravity = Gravity.CENTER
+        dateView.isSingleLine = true
+        dateView.maxLines = 1
+
+        //counter view settings
+        countMessagesView.layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+        countMessagesView.gravity = Gravity.CENTER_VERTICAL
+        countMessagesView.setPadding(0,0,0,0)
+        countMessagesView.isSingleLine = true
+        countMessagesView.maxLines = 1
+        countMessagesView.includeFontPadding = false
     }
 }
