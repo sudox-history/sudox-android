695aca6bd81192adb292570da0624670674fa6a3
Kotlinovsky <kotlinovsky@gmail.com>
2020-02-04 19:54:53 +0300

    [TASK-67] Moved fonts to design's submodule, also added styles for ListView's headers.

--- app/src/main/java/com/sudox/messenger/android/AppActivity.kt
+++ app/src/main/java/com/sudox/messenger/android/AppActivity.kt
@@ -3,6 +3,7 @@ package com.sudox.messenger.android
 import android.graphics.drawable.LayerDrawable
 import android.os.Bundle
 import android.view.KeyEvent
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.sudox.messenger.android.core.CoreActivity
 import com.sudox.messenger.android.core.CoreLoader
@@ -23,6 +24,10 @@ class AppActivity : AppCompatActivity(), CoreActivity {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (BuildConfig.DEBUG) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        }
 
         (window.decorView.background as LayerDrawable)
                 .getDrawable(1)
--- design/viewlist/src/main/java/com/sudox/design/viewlist/ViewList.kt
+++ design/viewlist/src/main/java/com/sudox/design/viewlist/ViewList.kt
@@ -1,15 +1,25 @@
 package com.sudox.design.viewlist
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
+import androidx.core.content.res.getResourceIdOrThrow
+import androidx.core.content.res.use
 import androidx.recyclerview.widget.RecyclerView
 
 class ViewList : RecyclerView {
 
-    internal var headerTextAppearance = R.style.TextAppearance_AppCompat
-    internal var footerTextAppearance = R.style.TextAppearance_AppCompat
+    internal var headerTextAppearance = 0
+    internal var footerTextAppearance = 0
 
-    constructor(context: Context) : super(context)
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : super(context, attrs, defStyle)
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, R.attr.viewListStyle)
+
+    @SuppressLint("Recycle")
+    constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : super(context, attrs, defStyle) {
+        context.obtainStyledAttributes(attrs, R.styleable.ViewList, defStyle, 0).use {
+            headerTextAppearance = it.getResourceIdOrThrow(R.styleable.ViewList_headerTextAppearance)
+            footerTextAppearance = it.getResourceIdOrThrow(R.styleable.ViewList_footerTextAppearance)
+        }
+    }
 }
