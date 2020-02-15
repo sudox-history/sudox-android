9b32a66d6386d929f1ff2140b174bdc5601bc882
Kotlinovsky <kotlinovsky@gmail.com>
2020-02-15 18:44:27 +0300

    [TASK-67] New test data.

--- people/peopletab/src/main/java/com/sudox/messenger/android/people/peopletab/PeopleTabFragment.kt
+++ people/peopletab/src/main/java/com/sudox/messenger/android/people/peopletab/PeopleTabFragment.kt
@@ -15,6 +15,7 @@ import com.sudox.messenger.android.people.peopletab.adapters.PeopleTabAdapter
 import com.sudox.messenger.android.people.peopletab.vos.AddedFriendVO
 import com.sudox.messenger.android.people.peopletab.vos.FriendRequestVO
 import com.sudox.messenger.android.people.peopletab.vos.MaybeYouKnowVO
+import com.sudox.messenger.android.people.peopletab.vos.SubscriptionVO
 import kotlinx.android.synthetic.main.fragment_people_tab.peopleTabContentList
 
 class PeopleTabFragment : CoreFragment(), ViewPagerFragment, ApplicationBarListener {
@@ -32,34 +33,39 @@ class PeopleTabFragment : CoreFragment(), ViewPagerFragment, ApplicationBarListe
             viewList = peopleTabContentList
 
             friendsRequestsVO.apply {
-                add(FriendRequestVO(4, "Pidor Request", 145, 2, null, 2L))
-                add(FriendRequestVO(5, "Pidor Request", 145, 2, "Hello, Pidor", 1L))
-                add(FriendRequestVO(5, "Pidor Request", 145, 2, "Hello, Pidor", 1L))
-                add(FriendRequestVO(5, "Pidor Request", 145, 2, "Hello, Pidor", 1L))
-                add(FriendRequestVO(5, "Pidor Request", 145, 2, "Hello, Pidor", 1L))
-                add(FriendRequestVO(5, "Pidor Request", 145, 2, "Hello, Pidor", 1L))
-                add(FriendRequestVO(6, "Pidor Request Last", 146, 2, null, 3L))
+                add(FriendRequestVO(4, "Pidor Request 1", 145, 2, null, 2L))
+                add(FriendRequestVO(5, "Pidor Request 2", 145, 2, "Hello, Pidor", 1L))
+                add(FriendRequestVO(5, "Pidor Request 3", 145, 2, "Hello, Pidor", 1L))
+                add(FriendRequestVO(5, "Pidor Request 4", 145, 2, "Hello, Pidor", 1L))
+                add(FriendRequestVO(5, "Pidor Request 5", 145, 2, "Hello, Pidor", 1L))
+                add(FriendRequestVO(5, "Pidor Request 6", 145, 2, "Hello, Pidor", 1L))
+                add(FriendRequestVO(6, "Pidor Request 7", 146, 2, null, 3L))
+            }
+
+            subscriptionsVOs.apply {
+                add(SubscriptionVO(1, "TheMax", SEEN_TIME_ONLINE, 45, "Suka, rabotai", 1, 1))
+                add(SubscriptionVO(1, "Not online", 45, 45, "Suka, rabotai", 2, 2))
             }
 
             addedFriendsVOs.apply {
-                add(AddedFriendVO(1, "Pidor Added", 145, 2, 1))
-                add(AddedFriendVO(2, "Pidor Added", 145, 2, 2))
-                add(AddedFriendVO(3, "Pidor Added", 145, 2, 3))
-                add(AddedFriendVO(4, "Pidor Added", 145, 2, 4))
-                add(AddedFriendVO(5, "Pidor Added", 145, 2, 5))
-                add(AddedFriendVO(6, "Pidor Added", 145, 2, 6))
+                add(AddedFriendVO(1, "Pidor Added 1", 145, 2, 1))
+                add(AddedFriendVO(2, "Pidor Added 2", 145, 2, 2))
+                add(AddedFriendVO(3, "Pidor Added 3", 145, 2, 3))
+                add(AddedFriendVO(4, "Pidor Added 4", SEEN_TIME_ONLINE, 2, 4))
+                add(AddedFriendVO(5, "Pidor Added 5", 145, 2, 5))
+                add(AddedFriendVO(6, "Pidor Added 6", 145, 2, 6))
             }
 
             maybeYouKnowAdapter.maybeYouKnowVOs.apply {
-                add(MaybeYouKnowVO(1, "Pidor", SEEN_TIME_ONLINE, 1, 15))
-                add(MaybeYouKnowVO(2, "Pidor", SEEN_TIME_ONLINE, 1, 18))
-                add(MaybeYouKnowVO(3, "Pidor", SEEN_TIME_ONLINE, 1, 18))
-                add(MaybeYouKnowVO(4, "Pidor", SEEN_TIME_ONLINE, 1, 18))
-                add(MaybeYouKnowVO(5, "Pidor", SEEN_TIME_ONLINE, 1, 18))
-                add(MaybeYouKnowVO(6, "Pidor", SEEN_TIME_ONLINE, 1, 18))
-                add(MaybeYouKnowVO(7, "Pidor", SEEN_TIME_ONLINE, 1, 18))
-                add(MaybeYouKnowVO(8, "Pidor", SEEN_TIME_ONLINE, 1, 18))
-                add(MaybeYouKnowVO(9, "Pidor", SEEN_TIME_ONLINE, 1, 18))
+                add(MaybeYouKnowVO(1, "Pidor 1", SEEN_TIME_ONLINE, 1, 15))
+                add(MaybeYouKnowVO(2, "Pidor 2", SEEN_TIME_ONLINE, 1, 18))
+                add(MaybeYouKnowVO(3, "Pidor 3", SEEN_TIME_ONLINE, 1, 18))
+                add(MaybeYouKnowVO(4, "Pidor 4", SEEN_TIME_ONLINE, 1, 18))
+                add(MaybeYouKnowVO(5, "Pidor 5", SEEN_TIME_ONLINE, 1, 18))
+                add(MaybeYouKnowVO(6, "Pidor 6", SEEN_TIME_ONLINE, 1, 18))
+                add(MaybeYouKnowVO(7, "Pidor 7", SEEN_TIME_ONLINE, 1, 18))
+                add(MaybeYouKnowVO(8, "Pidor 8", SEEN_TIME_ONLINE, 1, 18))
+                add(MaybeYouKnowVO(9, "Pidor 9", SEEN_TIME_ONLINE, 1, 18))
             }
         }
 
