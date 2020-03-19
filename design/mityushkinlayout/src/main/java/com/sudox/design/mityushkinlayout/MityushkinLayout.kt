6d50a010d95ca714cca4585d9e54439fb33a7ef5
Kotlinovsky <kotlinovsky@gmail.com>
2020-03-19 17:39:13 +0300

    [TASK-62] Created module for MityushkinLayout.

---
+++ design/mityushkinlayout/src/main/java/com/sudox/design/mityushkinlayout/MityushkinLayout.kt
@@ -0,0 +1,16 @@
+package com.sudox.design.mityushkinlayout
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.ViewGroup
+
+class MityushkinLayout : ViewGroup {
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
