74c4ddf9867c21cc0b5f88d178e1a19598b8197a
Kotlinovsky <kotlinovsky@gmail.com>
2020-03-23 21:04:56 +0300

    [TASK-62] Improved number formatting.

--- media/texts/src/main/java/com/sudox/messenger/android/media/texts/helpers/TextsHelper.kt
+++ media/texts/src/main/java/com/sudox/messenger/android/media/texts/helpers/TextsHelper.kt
@@ -11,13 +11,21 @@ import com.sudox.messenger.android.text.R
  * @param number Форматируемое число
  */
 fun formatNumber(context: Context, number: Int): String {
-    return if (number in 0 .. 999) {
-        number.toString()
-    } else if (number in 1000 .. 999_999) {
-        String.format("%.1f${context.getString(R.string.thousands_suffix)}", number / 1000F)
-    } else if (number in 1_000_000 .. 999_999_999) {
-        String.format("%.1f${context.getString(R.string.millions_suffix)}", number / 1_000_000F)
+    if (number in 0 .. 999) {
+        return number.toString()
+    }
+
+    val mask = if (number % 1000 == 0) {
+        "%.0f"
     } else {
-        String.format("%.1f${context.getString(R.string.billions_suffix)}", number / 1_000_000_000F)
+        "%.1f"
+    }
+
+    return if (number in 1000 .. 999_999) {
+        String.format("$mask${context.getString(R.string.thousands_suffix)}", number / 1000F)
+    } else if (number in 1_000_000 .. 999_999_999) {
+        String.format("$mask${context.getString(R.string.millions_suffix)}", number / 1_000_000F)
+    } else {
+        String.format("$mask${context.getString(R.string.billions_suffix)}", number / 1_000_000_000F)
     }
 }
