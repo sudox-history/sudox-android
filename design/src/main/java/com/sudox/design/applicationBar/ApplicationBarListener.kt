3a49ea170180873d9017c3afd2e3e7438f0a81bd
Kotlinovsky <kotlinovsky@gmail.com>
2020-03-29 12:51:11 +0300

    [TASK-161] Removed old components.

--- design/src/main/java/com/sudox/design/applicationBar/ApplicationBarListener.kt
+++
@@ -1,5 +0,0 @@
-package com.sudox.design.applicationBar
-
-interface ApplicationBarListener {
-    fun onButtonClicked(tag: Int)
-}
--- design/src/main/java/com/sudox/design/navigationBar/NavigationBarListener.kt
+++
@@ -1,5 +0,0 @@
-package com.sudox.design.navigationBar
-
-interface NavigationBarListener {
-    fun onButtonClicked(tag: Int)
-}
