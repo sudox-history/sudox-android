393f708bda8ecb1ab0de3d0849513c362b892556
Kotlinovsky <kotlinovsky@gmail.com>
2020-04-01 03:52:59 +0300

    [TASK-69] Removed unused resources & classes.

--- design/src/main/java/com/sudox/design/lists/sortedList/decorations/StickyLettersProvider.kt
+++
@@ -1,7 +0,0 @@
-package com.sudox.design.lists.sortedList.decorations
-
-import android.content.Context
-
-interface StickyLettersProvider {
-    fun getLetters(context: Context): HashMap<Int, String>
-}
