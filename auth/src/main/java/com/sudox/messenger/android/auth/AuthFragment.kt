45eae46f3aa1bced7d9034742ba9ea9c0c85447c
Kotlinovsky <kotlinovsky@gmail.com>
2020-04-04 20:24:49 +0300

    [TASK-69] Authorization now supports scrolling.

--- auth/src/main/java/com/sudox/messenger/android/auth/AuthFragment.kt
+++ auth/src/main/java/com/sudox/messenger/android/auth/AuthFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ScrollView
 import com.sudox.messenger.android.auth.views.AuthScreenLayout
 import com.sudox.messenger.android.auth.vos.AuthScreenVO
 import com.sudox.messenger.android.core.CoreFragment
 
+const val SCROLL_VIEW_ID = "scroll_view_id"
 const val AUTH_FRAGMENT_LAYOUT_ID_KEY = "auth_fragment_layout_id"
 
 open class AuthFragment<T : AuthScreenVO> : CoreFragment() {
@@ -15,9 +17,13 @@ open class AuthFragment<T : AuthScreenVO> : CoreFragment() {
     var screenVO: T? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return AuthScreenLayout(context!!).apply {
-            id = savedInstanceState?.getInt(AUTH_FRAGMENT_LAYOUT_ID_KEY, View.generateViewId()) ?: View.generateViewId()
-            vo = screenVO
+        return ScrollView(context).apply {
+            id = savedInstanceState?.getInt(SCROLL_VIEW_ID, View.generateViewId()) ?: View.generateViewId()
+
+            addView(AuthScreenLayout(context!!).apply {
+                id = savedInstanceState?.getInt(AUTH_FRAGMENT_LAYOUT_ID_KEY, View.generateViewId()) ?: View.generateViewId()
+                vo = screenVO
+            })
         }
     }
 
@@ -25,7 +31,8 @@ open class AuthFragment<T : AuthScreenVO> : CoreFragment() {
         super.onSaveInstanceState(outState)
 
         if (view != null) {
-            outState.putInt(AUTH_FRAGMENT_LAYOUT_ID_KEY, view!!.id)
+            outState.putInt(SCROLL_VIEW_ID, view!!.id)
+            outState.putInt(AUTH_FRAGMENT_LAYOUT_ID_KEY, (view as ViewGroup).getChildAt(0).id)
         }
     }
 }
