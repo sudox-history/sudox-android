200a5eea5b76f7d693e0eef44732b21831b35c29
Kotlinovsky <kotlinovsky@gmail.com>
2020-04-05 23:11:06 +0300

    [MASTER] Created module for auth API #2.

--- api/src/main/java/com/sudox/api/SudoxApiErrors.kt
+++ api/src/main/java/com/sudox/api/SudoxApiErrors.kt
@@ -3,4 +3,4 @@ package com.sudox.api
 const val OK_ERROR_CODE = 0
 const val SERVICE_UNAVAILABLE_ERROR_CODE = 1
 const val ACCESS_DENIED_ERROR_CODE = 2
-const val REQUEST_FORMAT_INVALID__CODE = 3
+const val REQUEST_FORMAT_INVALID_CODE = 3
