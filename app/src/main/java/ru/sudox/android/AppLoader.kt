6b5ddc2d94ad18f1d8cf96e72ee5a5ec52b7c60a
Kotlinovsky <kotlinovsky@gmail.com>
2020-04-07 00:03:59 +0300

    Revert "[MASTER] Implemented reconnection."
    This reverts commit 0fedc928

--- app/src/main/java/ru/sudox/android/AppLoader.kt
+++ app/src/main/java/ru/sudox/android/AppLoader.kt
@@ -4,15 +4,14 @@ import android.app.Application
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import io.michaelrocks.libphonenumber.android.PhoneNumberUtil
-import io.reactivex.rxjava3.disposables.Disposable
+import ru.sudox.api.SudoxApiImpl
+import ru.sudox.api.connections.impl.WebSocketConnection
+import ru.sudox.api.inject.ApiModule
 import ru.sudox.android.countries.inject.CountriesModule
 import ru.sudox.android.inject.DaggerLoaderComponent
 import ru.sudox.android.inject.LoaderComponent
+import io.michaelrocks.libphonenumber.android.PhoneNumberUtil
 import ru.sudox.api.common.SudoxApi
-import ru.sudox.api.common.SudoxApiStatus
-import ru.sudox.api.connections.impl.WebSocketConnection
-import ru.sudox.api.inject.ApiModule
 import javax.inject.Inject
 
 class AppLoader : Application() {
@@ -20,8 +19,6 @@ class AppLoader : Application() {
     companion object {
         var loaderComponent: LoaderComponent? = null
     }
-
-    private var apiStatusDisposable: Disposable? = null
 
     @Inject
     @JvmField
@@ -39,20 +36,10 @@ class AppLoader : Application() {
                 )).build()
 
         loaderComponent!!.inject(this)
-
-        sudoxApi!!.let {
-            apiStatusDisposable = it.statusSubject.subscribe { status ->
-                if (status == SudoxApiStatus.DISCONNECTED) {
-                    it.startConnection()
-                }
-            }
-
-            it.startConnection()
-        }
+        sudoxApi!!.startConnection()
     }
 
     override fun onTerminate() {
-        apiStatusDisposable!!.dispose()
         sudoxApi!!.endConnection()
         super.onTerminate()
     }
