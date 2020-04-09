cc21c2f1e1f9850de5b6cc48ac4bfac623ae618c
Kotlinovsky <kotlinovsky@gmail.com>
2020-04-09 20:05:08 +0300

    [MASTER] Removed unused logging.

--- api/src/main/java/ru/sudox/api/SudoxApiErrors.kt
+++ api/src/main/java/ru/sudox/api/SudoxApiErrors.kt
@@ -44,8 +44,6 @@ val errorNames = hashMapOf(
  * @param callback Кэллбэк для передачи кода ошибки.
  */
 inline fun createApiErrorsCallback(crossinline callback: (Int) -> (Unit)): (Throwable) -> (Unit) = {
-    Log.e("Sudox Error!", it.localizedMessage ?: "Without message")
-
     if (it is ApiException) {
         callback(it.code)
     } else if (it is IOException) {
