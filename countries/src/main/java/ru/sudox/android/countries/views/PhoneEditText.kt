9869f8609fc48abad54a7983970139696a980718
Kotlinovsky <kotlinovsky@gmail.com>
2020-04-12 19:42:16 +0300

    [TASK-22] Fixed PhoneEditText state saving.

--- countries/src/main/java/ru/sudox/android/countries/views/PhoneEditText.kt
+++ countries/src/main/java/ru/sudox/android/countries/views/PhoneEditText.kt
@@ -124,6 +124,7 @@ class PhoneEditText : SaveableViewGroup<PhoneEditText, PhoneEditTextState>, Edit
                 countrySelector.setCompoundDrawablesWithIntrinsicBounds(value.flagId, 0, 0, 0)
                 countrySelector.text = "+${value.countryCode}"
 
+                // TODO: Сброс только если ошибка связана с страной
                 (parent as EditTextLayout).errorText = null
             }
 
@@ -132,6 +133,7 @@ class PhoneEditText : SaveableViewGroup<PhoneEditText, PhoneEditTextState>, Edit
             invalidate()
         }
 
+    var ignoreCountryFromState = false
     var autofillMyNumber = false
         set(value) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
