067f1446d877f0f94adaea6aaa6ae131321b1cf1
Kotlinovsky <kotlinovsky@gmail.com>
2020-04-22 00:51:28 +0300

    [MASTER] Removed unused headers.

--- cryptography/src/main/jni/build.sh
+++ cryptography/src/main/jni/build.sh
@@ -9,14 +9,13 @@ sh ./dist-build/android-x86_64.sh
 sh ./dist-build/android-armv7-a.sh
 sh ./dist-build/android-armv8-a.sh
 
-rm -R ../includes/*/*
+rm -R ../include/*
 rm -R ../libs/*/*
 
-mv libsodium-android-i686/include/* ../includes/x86/
-mv libsodium-android-westmere/include/* ../includes/x86_64/
-mv libsodium-android-armv7-a/include/* ../includes/armeabi-v7a/
-mv libsodium-android-armv8-a/include/* ../includes/arm64-v8a/
+# Headers
+mv libsodium-android-westmere/include/* ../include/
 
+# Libs
 mv libsodium-android-i686/lib/libsodium.a ../libs/x86/libsodium.a
 mv libsodium-android-westmere/lib/libsodium.a ../libs/x86_64/libsodium.a
 mv libsodium-android-armv7-a/lib/libsodium.a ../libs/armeabi-v7a/libsodium.a
