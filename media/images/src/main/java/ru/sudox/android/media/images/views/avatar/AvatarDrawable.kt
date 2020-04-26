639ef398aee640a9c3dd127695adf66b8d251463
Kotlinovsky <kotlinovsky@gmail.com>
2020-04-26 09:55:39 +0300

    [TASK-85] AvatarDrawable now supports transition animations.

--- media/images/src/main/java/ru/sudox/android/media/images/views/avatar/AvatarDrawable.kt
+++ media/images/src/main/java/ru/sudox/android/media/images/views/avatar/AvatarDrawable.kt
@@ -1,21 +1,36 @@
 package ru.sudox.android.media.images.views.avatar
 
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.ColorFilter
 import android.graphics.PixelFormat
 import android.graphics.drawable.Drawable
+import androidx.core.graphics.ColorUtils
 
 class AvatarDrawable(
         val imageView: AvatarImageView
 ) : Drawable() {
 
+    private var currentAlpha = 255
+
     override fun draw(canvas: Canvas) {
-        canvas.drawColor(imageView.avatarColor)
+        canvas.drawColor(ColorUtils.setAlphaComponent(imageView.avatarColor, currentAlpha))
 
         val textY = bounds.exactCenterY() - imageView.avatarTextBounds.exactCenterY()
         val textX = bounds.exactCenterX() - imageView.avatarTextBounds.exactCenterX()
+        val color = imageView.avatarTextPaint.color
 
+        imageView.avatarTextPaint.color = ColorUtils.setAlphaComponent(color, currentAlpha)
         canvas.drawText(imageView.textInAvatar!!, textX, textY, imageView.avatarTextPaint)
+        imageView.avatarTextPaint.color = color
+    }
+
+    override fun setAlpha(alpha: Int) {
+        currentAlpha = alpha
+    }
+
+    override fun getAlpha(): Int {
+        return currentAlpha
     }
 
     override fun getOpacity(): Int {
@@ -23,8 +38,5 @@ class AvatarDrawable(
     }
 
     override fun setColorFilter(colorFilter: ColorFilter?) {
-    }
-
-    override fun setAlpha(alpha: Int) {
     }
 }
