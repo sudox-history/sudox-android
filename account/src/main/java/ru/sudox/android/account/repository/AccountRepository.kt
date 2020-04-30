e18e9dcab54df2a0e4a47d05db3572a3fb6268e1
Kotlinovsky <kotlinovsky@gmail.com>
2020-05-01 02:10:08 +0300

    [TASK-22] Added some logs and methods for AccountRepository.

--- account/src/main/java/ru/sudox/android/account/repository/AccountRepository.kt
+++ account/src/main/java/ru/sudox/android/account/repository/AccountRepository.kt
@@ -2,12 +2,12 @@ package ru.sudox.android.account.repository
 
 import android.accounts.Account
 import android.accounts.AccountManager
-import android.content.Context
 import android.os.Bundle
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
 import android.util.Base64
 import android.util.Log
+import ru.sudox.android.account.BuildConfig
 import java.security.KeyStore
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
@@ -47,11 +47,19 @@ class AccountRepository(
             doFinal(secret.toByteArray())
         }, Base64.DEFAULT)
 
-        accountManager.addAccountExplicitly(Account(name, accountType), encryptedSecret, Bundle().apply {
+        val result = accountManager.addAccountExplicitly(Account(name, accountType), encryptedSecret, Bundle().apply {
             putString(ACCOUNTMANAGER_ACCOUNT_SECRET_IV_KEY, Base64.encodeToString(cipher.iv, Base64.DEFAULT))
             putString(AccountManager.KEY_ACCOUNT_NAME, name)
             putString(ACCOUNTMANAGER_ACCOUNT_ID_KEY, id)
         })
+
+        if (BuildConfig.DEBUG) {
+            if (result) {
+                Log.d("Sudox Account", "Account $id with name $name successfully created")
+            } else {
+                Log.d("Sudox Account", "Account with name $name already exists!")
+            }
+        }
     }
 
     /**
@@ -61,19 +69,48 @@ class AccountRepository(
      * @return Токен аккаунта (null если не найден)
      */
     fun getAccountToken(id: String): String? {
-        val account = accountManager.getAccountsByType(accountType).find {
-            accountManager.getUserData(it, ACCOUNTMANAGER_ACCOUNT_ID_KEY) == id
-        } ?: return null
-
+        val account = findAccountById(id) ?: return null
         val encryptedSecret = Base64.decode(accountManager.getPassword(account), Base64.DEFAULT)
         val secretIv = Base64.decode(accountManager.getUserData(account, ACCOUNTMANAGER_ACCOUNT_SECRET_IV_KEY), Base64.DEFAULT)
 
-        val key = (keyStore.getEntry(getAccountKeyAlias(id), null) as KeyStore.SecretKeyEntry).secretKey
+        return try {
+            val key = (keyStore.getEntry(getAccountKeyAlias(id), null) as KeyStore.SecretKeyEntry).secretKey
 
-        return String(with(Cipher.getInstance("AES/CTR/NoPadding")) {
-            init(Cipher.DECRYPT_MODE, key, IvParameterSpec(secretIv))
-            doFinal(encryptedSecret)
-        })
+            String(with(Cipher.getInstance("AES/CTR/NoPadding")) {
+                init(Cipher.DECRYPT_MODE, key, IvParameterSpec(secretIv))
+                doFinal(encryptedSecret)
+            })
+        } catch (e: Exception) {
+            if (BuildConfig.DEBUG) {
+                Log.d("Sudox Account", "Error during account $id decryption. It will be removed!", e)
+            }
+
+            accountManager.removeAccountExplicitly(account)
+            null
+        }
+    }
+
+    /**
+     * Удаляет аккаунт из AccountManager по ID
+     * NB! Ключ шифрования тоже будет удален
+     *
+     * @param id ID аккаунта, который нужно удалить.
+     */
+    fun removeAccount(id: String) {
+        val account = findAccountById(id)
+
+        if (account != null) {
+            accountManager.removeAccountExplicitly(account)
+            Log.d("Sudox Account", "Account $id removed by user/app.")
+        } else if (BuildConfig.DEBUG) {
+            Log.d("Sudox Account", "Couldn't remove account $id because it not found.")
+        }
+    }
+
+    private fun findAccountById(id: String): Account? {
+        return accountManager.getAccountsByType(accountType).find {
+            accountManager.getUserData(it, ACCOUNTMANAGER_ACCOUNT_ID_KEY) == id
+        } ?: return null
     }
 
     /**
@@ -85,7 +122,17 @@ class AccountRepository(
         val id = accountManager.getUserData(account, ACCOUNTMANAGER_ACCOUNT_ID_KEY)
         val alias = getAccountKeyAlias(id)
 
-        keyStore.deleteEntry(alias)
+        try {
+            keyStore.deleteEntry(alias)
+
+            if (BuildConfig.DEBUG) {
+                Log.d("Sudox Account", "Key of account $id successfully removed.")
+            }
+        } catch (e: Exception) {
+            if (BuildConfig.DEBUG) {
+                Log.d("Sudox Account", "Error during account $id key removing!", e)
+            }
+        }
     }
 
     private fun getAccountKeyAlias(id: String) = "sudox_account_${id}_key"
